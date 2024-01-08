66b25593b80065343e14210411db2a17b54928ac
Enrique Izel <eleaoizel@gmail.com>
2024-01-08 16:00:00 -0400

    feat: create retrofit initializer

---
+++ data/src/main/java/com/example/data/RetrofitInitializer.kt
@@ -0,0 +1,46 @@
+package com.example.data
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+class RetrofitInitializer {
+
+  companion object {
+    const val BASE_URL = "https://api.themoviedb.org/3/movie/"
+  }
+
+  private fun getLoggingInterceptor(): HttpLoggingInterceptor {
+    return HttpLoggingInterceptor().apply {
+      level = HttpLoggingInterceptor.Level.BODY
+    }
+  }
+
+  private fun getOkHttpClient(): OkHttpClient {
+    val builder = OkHttpClient.Builder()
+
+    builder
+      .addInterceptor(getLoggingInterceptor())
+      .readTimeout(30, TimeUnit.SECONDS)
+      .connectTimeout(30, TimeUnit.SECONDS)
+
+    return builder.build()
+  }
+
+  private fun getRetrofitClient(): Retrofit {
+    val builder = Retrofit.Builder()
+
+    builder
+      .baseUrl(BASE_URL)
+      .addConverterFactory(GsonConverterFactory.create())
+      .client(getOkHttpClient())
+
+    return builder.build()
+  }
+
+  fun createApiService(): ApiService {
+    return getRetrofitClient().create(ApiService::class.java)
+  }
+}
