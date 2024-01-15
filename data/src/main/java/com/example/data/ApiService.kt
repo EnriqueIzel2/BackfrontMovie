b84330f6a43d4c81c8875f4f75f6cee6b9a69534
Enrique Izel <eleaoizel@gmail.com>
2024-01-15 19:49:02 -0400

    feat: create method to get popular movies

--- data/src/main/java/com/example/data/ApiService.kt
+++ data/src/main/java/com/example/data/ApiService.kt
@@ -1,6 +1,14 @@
 package com.example.data
 
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ApiService {
 
+  companion object {
+    const val API_KEY = ""
+  }
 
+  @GET("popular")
+  fun getPopularMovie(@Query("api_key") api: String = API_KEY)
 }
