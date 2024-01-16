0106904425edf1d41d7785dfbeecfc7ed805cadf
Enrique Izel <eleaoizel@gmail.com>
2024-01-15 20:53:33 -0400

    feat: create data class

---
+++ data/src/main/java/com/example/data/app/repository/dto/GenreDTO.kt
@@ -0,0 +1,11 @@
+package com.example.data.app.repository.dto
+
+
+import com.google.gson.annotations.SerializedName
+
+data class GenreDTO(
+  @SerializedName("id")
+  val id: Int?,
+  @SerializedName("name")
+  val name: String?
+)
