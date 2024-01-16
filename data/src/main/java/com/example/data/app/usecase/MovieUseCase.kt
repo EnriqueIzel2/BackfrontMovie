e761e94c3fbe3f43741483cce371374516494107
Enrique Izel <eleaoizel@gmail.com>
2024-01-16 15:36:03 -0400

    feat: create MovieUseCase

---
+++ data/src/main/java/com/example/data/app/usecase/MovieUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.data.app.usecase
+
+import com.example.data.app.repository.model.Movie
+import com.example.data.commons.viewstate.ViewState
+
+interface MovieUseCase {
+  suspend fun getPopular(): ViewState<List<Movie>?>
+}
