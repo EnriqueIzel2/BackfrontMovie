175638ef0523af6c1590fc98fd8ff92b0b71aed1
Enrique Izel <eleaoizel@gmail.com>
2024-01-16 15:36:50 -0400

    feat: create implementation to MovieUseCase

---
+++ data/src/main/java/com/example/data/app/usecase/MovieUseCaseImpl.kt
@@ -0,0 +1,16 @@
+package com.example.data.app.usecase
+
+import com.example.data.app.repository.MovieRepository
+import com.example.data.app.repository.model.Movie
+import com.example.data.commons.mapper.MovieMapper
+import com.example.data.commons.viewstate.ViewState
+
+class MovieUseCaseImpl(private val repository: MovieRepository) : MovieUseCase {
+  override suspend fun getPopular(): ViewState<List<Movie>?> {
+    val response = repository.getPopular()
+
+    val movies = MovieMapper.getMovieResponse(response)
+
+    return ViewState.Success(movies)
+  }
+}
