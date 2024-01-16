e1268febf41ef14d3cf9a123c1165ef3dd5f9352
Enrique Izel <eleaoizel@gmail.com>
2024-01-16 15:35:37 -0400

    feat: add null for default value and create method to get MovieDTO to Movie

--- data/src/main/java/com/example/data/app/repository/dto/MovieDTO.kt
+++ data/src/main/java/com/example/data/app/repository/dto/MovieDTO.kt
@@ -1,57 +1,88 @@
 package com.example.data.app.repository.dto
 
 
+import com.example.data.app.repository.model.Movie
 import com.google.gson.annotations.SerializedName
 
 data class MovieDTO(
   @SerializedName("adult")
-  val adult: Boolean?,
+  val adult: Boolean? = null,
   @SerializedName("backdrop_path")
-  val backdropPath: String?,
+  val backdropPath: String? = null,
   @SerializedName("belongs_to_collection")
-  val belongsToCollection: Any?,
+  val belongsToCollection: Any? = null,
   @SerializedName("budget")
-  val budget: Int?,
+  val budget: Int? = null,
   @SerializedName("genres")
-  val genres: List<GenreDTO?>?,
+  val genres: List<GenreDTO?>? = null,
   @SerializedName("homepage")
-  val homepage: String?,
+  val homepage: String? = null,
   @SerializedName("id")
-  val id: Int?,
+  val id: Int? = null,
   @SerializedName("imdb_id")
-  val imdbId: String?,
+  val imdbId: String? = null,
   @SerializedName("original_language")
-  val originalLanguage: String?,
+  val originalLanguage: String? = null,
   @SerializedName("original_title")
-  val originalTitle: String?,
+  val originalTitle: String? = null,
   @SerializedName("overview")
-  val overview: String?,
+  val overview: String? = null,
   @SerializedName("popularity")
-  val popularity: Double?,
+  val popularity: Double? = null,
   @SerializedName("poster_path")
-  val posterPath: String?,
+  val posterPath: String? = null,
   @SerializedName("production_companies")
-  val productionCompanies: List<ProductionCompanyDTO?>?,
+  val productionCompanies: List<ProductionCompanyDTO?>? = null,
   @SerializedName("production_countries")
-  val productionCountries: List<ProductionCountryDTO?>?,
+  val productionCountries: List<ProductionCountryDTO?>? = null,
   @SerializedName("release_date")
-  val releaseDate: String?,
+  val releaseDate: String? = null,
   @SerializedName("revenue")
-  val revenue: Int?,
+  val revenue: Int? = null,
   @SerializedName("runtime")
-  val runtime: Int?,
+  val runtime: Int? = null,
   @SerializedName("spoken_languages")
-  val spokenLanguages: List<SpokenLanguageDTO?>?,
+  val spokenLanguages: List<SpokenLanguageDTO?>? = null,
   @SerializedName("status")
-  val status: String?,
+  val status: String? = null,
   @SerializedName("tagline")
-  val tagline: String?,
+  val tagline: String? = null,
   @SerializedName("title")
-  val title: String?,
+  val title: String? = null,
   @SerializedName("video")
-  val video: Boolean?,
+  val video: Boolean? = null,
   @SerializedName("vote_average")
-  val voteAverage: Double?,
+  val voteAverage: Double? = null,
   @SerializedName("vote_count")
-  val voteCount: Int?
+  val voteCount: Int? = null
 )
+
+fun MovieDTO.toMovie(): Movie {
+  return Movie(
+    adult,
+    backdropPath,
+    belongsToCollection,
+    budget,
+    genres,
+    homepage,
+    id,
+    imdbId,
+    originalLanguage,
+    originalTitle,
+    overview,
+    popularity,
+    posterPath,
+    productionCompanies,
+    productionCountries,
+    releaseDate,
+    revenue,
+    runtime,
+    spokenLanguages,
+    status,
+    tagline,
+    title,
+    video,
+    voteAverage,
+    voteCount
+  )
+}
