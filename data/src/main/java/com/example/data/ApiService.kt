3a4e7dca02cbe92411fb5388b167785798100832
Enrique Izel <eleaoizel@gmail.com>
2024-01-17 10:52:35 -0400

    feat: add api key

--- data/src/main/java/com/example/data/ApiService.kt
+++ data/src/main/java/com/example/data/ApiService.kt
@@ -1,15 +1,16 @@
 package com.example.data
 
 import com.example.data.app.repository.dto.MovieResultDTO
+import com.example.data.donotshare.DEFAULT_API_KEY
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
 
   companion object {
-    const val API_KEY = ""
+    const val API_KEY = DEFAULT_API_KEY
   }
 
   @GET("popular")
-  suspend fun getPopularMovie(@Query("api_key") api: String = API_KEY) : MovieResultDTO
+  suspend fun getPopularMovie(@Query("api_key") api: String = API_KEY): MovieResultDTO
 }
