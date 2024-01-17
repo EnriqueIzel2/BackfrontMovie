b61b37b3e3fdec2b86af6c1cfd2c0cad75032fe2
Enrique Izel <eleaoizel@gmail.com>
2024-01-17 10:53:27 -0400

    feat: create de MainViewModelFactory

---
+++ app/src/main/java/com/example/backfrontmovie/app/viewmodel/MainViewModelFactory.kt
@@ -0,0 +1,21 @@
+package com.example.backfrontmovie.app.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.data.RetrofitInitializer
+import com.example.data.app.repository.MovieRepositoryImpl
+import com.example.data.app.usecase.MovieUseCaseImpl
+
+class MainViewModelFactory : ViewModelProvider.Factory {
+  override fun <T : ViewModel> create(modelClass: Class<T>): T {
+    if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+      val service = RetrofitInitializer().createApiService()
+      val repository = MovieRepositoryImpl(service)
+      val useCase = MovieUseCaseImpl(repository)
+
+      return MainViewModel(useCase) as T
+    }
+
+    throw IllegalArgumentException("unknown view model class")
+  }
+}
