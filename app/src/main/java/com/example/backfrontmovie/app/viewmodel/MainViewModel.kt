47db528b607a25e670a8fcf2885dac05c561c22f
Enrique Izel <eleaoizel@gmail.com>
2024-01-17 09:44:51 -0400

    feat: create the MainViewModel

---
+++ app/src/main/java/com/example/backfrontmovie/app/viewmodel/MainViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.backfrontmovie.app.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
+import androidx.lifecycle.viewModelScope
+import com.example.data.app.repository.model.Movie
+import com.example.data.app.usecase.MovieUseCase
+import com.example.data.commons.viewstate.ViewState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainViewModel(private val useCase: MovieUseCase) : ViewModel() {
+
+  private val _popularMovies = MediatorLiveData<ViewState<List<Movie>?>>()
+  val popularMovies: LiveData<ViewState<List<Movie>?>> = _popularMovies.map { it }
+
+  fun getPopular() {
+    viewModelScope.launch(Dispatchers.IO) {
+      runCatching {
+        _popularMovies.value = ViewState.Loading(true)
+
+        val response = withContext(Dispatchers.IO) {
+          useCase.getPopular()
+        }
+
+        _popularMovies.value = response
+      }.onFailure {
+        _popularMovies.value = ViewState.Error(it)
+      }.also {
+        _popularMovies.value = ViewState.Loading(false)
+      }
+    }
+  }
+}
