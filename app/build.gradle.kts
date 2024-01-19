de1bfd0a83662066dae7fb10109323f5309d68bc
Enrique Izel <eleaoizel@gmail.com>
2024-01-19 11:53:51 -0400

    feat: add viewBinding and new dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,9 @@ android {
   kotlinOptions {
     jvmTarget = "1.8"
   }
+  viewBinding {
+    enable = true
+  }
 }
 
 dependencies {
@@ -42,6 +45,9 @@ dependencies {
   implementation("androidx.lifecycle:lifecycle-viewmodel:2.7.0")
   implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
   implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+  implementation("androidx.navigation:navigation-common:2.7.6")
+  implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+  implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
