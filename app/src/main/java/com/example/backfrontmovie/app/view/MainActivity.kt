cbfd0f8be762e009219cfbcf4baef7112d0024fa
Enrique Izel <eleaoizel@gmail.com>
2024-01-23 11:01:21 -0400

    refactor: remove modelView initialization for another file

--- app/src/main/java/com/example/backfrontmovie/app/view/MainActivity.kt
+++ app/src/main/java/com/example/backfrontmovie/app/view/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.example.backfrontmovie.app.view
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.backfrontmovie.R
-import com.example.backfrontmovie.app.viewmodel.MainViewModel
-import com.example.backfrontmovie.app.viewmodel.MainViewModelFactory
 import com.example.backfrontmovie.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,14 +19,6 @@ class MainActivity : AppCompatActivity() {
     setContentView(binding.root)
 
     setupBottomNavigation()
-
-    val viewModel = MainViewModelFactory().create(MainViewModel::class.java)
-
-    viewModel.popularMovies.observe(this) {
-      Log.i("Result", "onCreate: lista de filmes $it")
-    }
-
-    viewModel.getPopular()
   }
 
   private fun setupBottomNavigation() {
