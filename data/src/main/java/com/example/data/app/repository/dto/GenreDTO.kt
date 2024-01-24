3ae703363dda83c56232e0aab0c91387955afdaa
Enrique Izel <eleaoizel@gmail.com>
2024-01-24 16:55:29 -0400

    feat: add Parcelize

--- data/src/main/java/com/example/data/app/repository/dto/GenreDTO.kt
+++ data/src/main/java/com/example/data/app/repository/dto/GenreDTO.kt
@@ -1,11 +1,14 @@
 package com.example.data.app.repository.dto
 
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class GenreDTO(
   @SerializedName("id")
   val id: Int?,
   @SerializedName("name")
   val name: String?
-)
+) : Parcelable
