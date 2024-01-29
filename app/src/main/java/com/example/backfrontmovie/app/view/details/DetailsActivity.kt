f7f9d425f2629229af249614eefbcf5f21b77e31
Enrique Izel <eleaoizel@gmail.com>
2024-01-29 15:26:39 -0400

    feat: create function to add background

--- app/src/main/java/com/example/backfrontmovie/app/view/details/DetailsActivity.kt
+++ app/src/main/java/com/example/backfrontmovie/app/view/details/DetailsActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.example.backfrontmovie.databinding.ActivityDetailsBinding
 import com.example.data.app.repository.model.Movie
 
@@ -11,6 +12,7 @@ class DetailsActivity : AppCompatActivity() {
 
   private lateinit var binding: ActivityDetailsBinding
   private val detailsTitle by lazy { binding.detailsTitle }
+  private val detailsBackground by lazy { binding.detailsBackground }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -20,8 +22,18 @@ class DetailsActivity : AppCompatActivity() {
 
     intent.extras?.let {
       val item = it.getParcelable("item") as Movie?
-      detailsTitle.text = item?.title
+
+      setupItemView(item)
     }
+  }
+
+  private fun setupItemView(item: Movie?) {
+    supportActionBar?.title = item?.title
+    val postPath = "https://image.tmdb.org/t/p/w500${item?.backdropPath}"
+
+    Glide.with(detailsBackground.context).load(postPath).into(detailsBackground)
+
+    detailsTitle.text = item?.title
   }
 
   companion object {
