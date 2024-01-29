372e393f596a7ac297811eb988d4e0b1318ec27a
Enrique Izel <eleaoizel@gmail.com>
2024-01-29 17:20:11 -0400

    feat: add new components

--- app/src/main/java/com/example/backfrontmovie/app/view/details/DetailsActivity.kt
+++ app/src/main/java/com/example/backfrontmovie/app/view/details/DetailsActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
+import com.example.backfrontmovie.commons.extensions.formatToLatamDate
 import com.example.backfrontmovie.databinding.ActivityDetailsBinding
 import com.example.data.app.repository.model.Movie
 
@@ -13,6 +14,9 @@ class DetailsActivity : AppCompatActivity() {
   private lateinit var binding: ActivityDetailsBinding
   private val detailsTitle by lazy { binding.detailsTitle }
   private val detailsBackground by lazy { binding.detailsBackground }
+  private val detailsReleaseDate by lazy { binding.detailsReleaseDate }
+  private val detailsPoster by lazy { binding.detailsPoster }
+  private val detailsOverview by lazy { binding.detailsOverview }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -29,11 +33,15 @@ class DetailsActivity : AppCompatActivity() {
 
   private fun setupItemView(item: Movie?) {
     supportActionBar?.title = item?.title
-    val postPath = "https://image.tmdb.org/t/p/w500${item?.backdropPath}"
+    val backgroundPath = "https://image.tmdb.org/t/p/w500${item?.backdropPath}"
+    val posterPath = "https://image.tmdb.org/t/p/w185${item?.posterPath}"
 
-    Glide.with(detailsBackground.context).load(postPath).into(detailsBackground)
+    Glide.with(detailsBackground.context).load(backgroundPath).into(detailsBackground)
+    Glide.with(detailsPoster.context).load(posterPath).into(detailsPoster)
 
     detailsTitle.text = item?.title
+    detailsReleaseDate.text = item?.releaseDate?.formatToLatamDate()
+    detailsOverview.text = item?.overview
   }
 
   companion object {
