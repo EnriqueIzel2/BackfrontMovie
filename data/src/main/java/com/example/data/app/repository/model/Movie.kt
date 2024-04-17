a888048540721209b73f056811f887af4d9018e2
Enrique Izel <eleaoizel@gmail.com>
2024-04-17 09:33:57 -0400

    feat: add annotations for create entity and primary key

--- data/src/main/java/com/example/data/app/repository/model/Movie.kt
+++ data/src/main/java/com/example/data/app/repository/model/Movie.kt
@@ -1,6 +1,8 @@
 package com.example.data.app.repository.model
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.example.data.app.repository.dto.GenreDTO
 import com.example.data.app.repository.dto.ProductionCompanyDTO
 import com.example.data.app.repository.dto.ProductionCountryDTO
@@ -9,14 +11,16 @@ import kotlinx.parcelize.Parcelize
 import kotlinx.parcelize.RawValue
 
 @Parcelize
+@Entity("movie")
 data class Movie(
+  @PrimaryKey
+  val id: Int? = null,
   val adult: Boolean? = null,
   val backdropPath: String? = null,
   val belongsToCollection: @RawValue Any? = null,
   val budget: Int? = null,
   val genres: List<GenreDTO?>? = null,
   val homepage: String? = null,
-  val id: Int? = null,
   val imdbId: String? = null,
   val originalLanguage: String? = null,
   val originalTitle: String? = null,
