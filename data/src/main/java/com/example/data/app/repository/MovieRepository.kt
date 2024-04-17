3aee16b89727b0716f2c51318170387635231415
Enrique Izel <eleaoizel@gmail.com>
2024-04-17 10:01:59 -0400

    feat: create functions to insert and get movies in database

--- data/src/main/java/com/example/data/app/repository/MovieRepository.kt
+++ data/src/main/java/com/example/data/app/repository/MovieRepository.kt
@@ -1,9 +1,12 @@
 package com.example.data.app.repository
 
 import com.example.data.app.repository.dto.MovieResultDTO
+import com.example.data.app.repository.model.Movie
 
 interface MovieRepository {
   suspend fun getPopular(): MovieResultDTO
-
   suspend fun getTopRated(): MovieResultDTO
+
+  suspend fun insertMovie(movie: Movie)
+  suspend fun getMovies(): List<Movie>?
 }
