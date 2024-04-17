0c88cf737475c459cca40966dd562f1a16f71d28
Enrique Izel <eleaoizel@gmail.com>
2024-04-17 09:34:25 -0400

    feat: add entity

--- data/src/main/java/com/example/data/app/datasource/local/AppDatabase.kt
+++ data/src/main/java/com/example/data/app/datasource/local/AppDatabase.kt
@@ -4,9 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.data.app.repository.model.Movie
 
 @Database(
-  entities = [],
+  entities = [Movie::class],
   version = 1,
   exportSchema = false
 )
