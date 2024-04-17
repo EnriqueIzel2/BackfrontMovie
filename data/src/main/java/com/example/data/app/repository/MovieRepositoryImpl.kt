edec4eebb9079992cca4e63c28df68435597e751
Enrique Izel <eleaoizel@gmail.com>
2024-04-17 10:02:48 -0400

    feat: create functions to insert and get movies in database

--- data/src/main/java/com/example/data/app/repository/MovieRepositoryImpl.kt
+++ data/src/main/java/com/example/data/app/repository/MovieRepositoryImpl.kt
@@ -1,9 +1,14 @@
 package com.example.data.app.repository
 
+import com.example.data.app.datasource.local.dao.AppDAO
 import com.example.data.app.datasource.remote.ApiService
 import com.example.data.app.repository.dto.MovieResultDTO
+import com.example.data.app.repository.model.Movie
 
-class MovieRepositoryImpl(private val remoteDataSource: ApiService) : MovieRepository {
+class MovieRepositoryImpl(
+  private val remoteDataSource: ApiService,
+  private val localDataSource: AppDAO
+) : MovieRepository {
 
   override suspend fun getPopular(): MovieResultDTO {
     return remoteDataSource.getPopularMovie()
@@ -11,5 +16,13 @@ class MovieRepositoryImpl(private val remoteDataSource: ApiService) : MovieRepos
 
   override suspend fun getTopRated(): MovieResultDTO {
     return remoteDataSource.getTopRated()
+  }
+
+  override suspend fun insertMovie(movie: Movie) {
+    return localDataSource.insertMovie(movie)
+  }
+
+  override suspend fun getMovies(): List<Movie>? {
+    return localDataSource.getMovies()
   }
 }
