7089534c5e69a6ffb0f194c0ef9900dc22152207
Enrique Izel <eleaoizel@gmail.com>
2024-04-19 08:47:48 -0400

    feat: add configuration to database

--- app/src/main/java/com/example/backfrontmovie/app/viewmodel/MainViewModelFactory.kt
+++ app/src/main/java/com/example/backfrontmovie/app/viewmodel/MainViewModelFactory.kt
@@ -1,16 +1,20 @@
 package com.example.backfrontmovie.app.viewmodel
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.data.app.datasource.local.AppDatabase
 import com.example.data.app.datasource.remote.RetrofitInitializer
 import com.example.data.app.repository.MovieRepositoryImpl
 import com.example.data.app.usecase.MovieUseCaseImpl
 
-class MainViewModelFactory : ViewModelProvider.Factory {
+class MainViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
   override fun <T : ViewModel> create(modelClass: Class<T>): T {
     if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+      val appDatabase = AppDatabase.buildDatabase(context)
+      val appDao = appDatabase.appDAO
       val service = RetrofitInitializer().createApiService()
-      val repository = MovieRepositoryImpl(service)
+      val repository = MovieRepositoryImpl(service, appDao)
       val useCase = MovieUseCaseImpl(repository)
 
       return MainViewModel(useCase) as T
