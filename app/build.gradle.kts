6d51440b092cfdc902927fcb34d79e31abf26095
Enrique Izel <eleaoizel@gmail.com>
2024-04-19 08:46:14 -0400

    feat: add new dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
   id("com.android.application")
   id("org.jetbrains.kotlin.android")
+  id("kotlin-kapt")
 }
 
 android {
@@ -52,4 +53,7 @@ dependencies {
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+  kapt("androidx.room:room-compiler:2.6.1")
+  implementation("androidx.room:room-common:2.6.1")
+  implementation("androidx.room:room-ktx:2.6.1")
 }
--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
   id("com.android.library")
   id("org.jetbrains.kotlin.android")
   id("kotlin-parcelize")
+  id("kotlin-kapt")
 }
 
 android {
@@ -38,7 +39,7 @@ dependencies {
   implementation("com.squareup.retrofit2:retrofit:2.9.0")
   implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.12")
   implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-  implementation("androidx.room:room-compiler:2.6.1")
+  kapt("androidx.room:room-compiler:2.6.1")
   implementation("androidx.room:room-common:2.6.1")
   implementation("androidx.room:room-ktx:2.6.1")
   testImplementation("junit:junit:4.13.2")
