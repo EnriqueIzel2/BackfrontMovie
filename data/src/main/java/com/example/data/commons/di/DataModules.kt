3ad095e56a8ae131c8a9fe71f4b26fda322da7e4
Enrique Izel <eleaoizel@gmail.com>
2024-05-14 14:02:34 -0400

    feat: create the modules file

---
+++ data/src/main/java/com/example/data/commons/di/DataModules.kt
@@ -0,0 +1,42 @@
+package com.example.data.commons.di
+
+import android.content.Context
+import com.example.data.app.datasource.local.AppDatabase
+import com.example.data.app.datasource.local.dao.AppDAO
+import com.example.data.app.datasource.remote.RetrofitInitializer
+import com.example.data.app.repository.MovieRepository
+import com.example.data.app.repository.MovieRepositoryImpl
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+
+private val remoteServiceModule = module {
+  single { RetrofitInitializer().createApiService() }
+}
+
+private val localServiceModule = module {
+  fun provideDatabase(context: Context): AppDatabase {
+    return AppDatabase.buildDatabase(context)
+  }
+
+  fun provideDao(appDatabase: AppDatabase): AppDAO {
+    return appDatabase.appDAO
+  }
+
+  single { provideDatabase(androidContext()) }
+  single { provideDao(get()) }
+}
+
+private val repositoryModule = module {
+  factory<MovieRepository> { MovieRepositoryImpl(get(), get()) }
+}
+
+private val useCaseModule = module {
+  single<MovieRepository> { MovieRepositoryImpl(get(), get()) }
+}
+
+fun getDataModules() =  listOf(
+  remoteServiceModule,
+  localServiceModule,
+  repositoryModule,
+  useCaseModule
+)
