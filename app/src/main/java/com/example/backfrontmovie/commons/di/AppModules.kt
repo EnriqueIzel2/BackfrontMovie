004e375f01be00197feabf7525fec63eaea8df8f
Enrique Izel <eleaoizel@gmail.com>
2024-06-03 14:39:04 -0400

    feat: add login module

--- app/src/main/java/com/example/backfrontmovie/commons/di/AppModules.kt
+++ app/src/main/java/com/example/backfrontmovie/commons/di/AppModules.kt
@@ -1,11 +1,13 @@
 package com.example.backfrontmovie.commons.di
 
+import com.example.backfrontmovie.app.viewmodel.LoginViewModel
 import com.example.backfrontmovie.app.viewmodel.MainViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 private val viewModelModules = module {
   viewModel { MainViewModel(get()) }
+  viewModel { LoginViewModel() }
 }
 
 fun getAppModules() = listOf(viewModelModules)
