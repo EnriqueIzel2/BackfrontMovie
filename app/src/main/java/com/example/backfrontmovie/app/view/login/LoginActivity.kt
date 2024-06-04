aab2cc3e6daf5e1b16f93df81b253cb92eae327f
Enrique Izel <eleaoizel@gmail.com>
2024-06-04 15:32:13 -0400

    WIP: add function to handle the click on login button

--- app/src/main/java/com/example/backfrontmovie/app/view/login/LoginActivity.kt
+++ app/src/main/java/com/example/backfrontmovie/app/view/login/LoginActivity.kt
@@ -3,8 +3,8 @@ package com.example.backfrontmovie.app.view.login
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.example.backfrontmovie.R
 import com.example.backfrontmovie.app.viewmodel.LoginViewModel
+import com.example.backfrontmovie.databinding.ActivityLoginBinding
 import com.example.data.commons.viewstate.ViewState
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -12,10 +12,28 @@ class LoginActivity : AppCompatActivity() {
 
   private val loginViewModel: LoginViewModel by viewModel()
 
+  private lateinit var binding: ActivityLoginBinding
+  private val loginButton by lazy { binding.loginButton }
+
   override fun onStart() {
     super.onStart()
 
     setObservable()
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    binding = ActivityLoginBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    handleLoginButton()
+  }
+
+  private fun handleLoginButton() {
+    loginButton.setOnClickListener {
+      loginViewModel.login(this, "teste@teste.com", "123456")
+    }
   }
 
   private fun setObservable() {
@@ -32,10 +50,5 @@ class LoginActivity : AppCompatActivity() {
     }
 
     loginViewModel.onStart()
-  }
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_login)
   }
 }
