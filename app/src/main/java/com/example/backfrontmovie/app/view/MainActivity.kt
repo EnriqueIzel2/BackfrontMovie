a50638c7b64a5ea83d7d1bfc63fd91e9466ae81a
Enrique Izel <eleaoizel@gmail.com>
2024-06-07 15:24:14 -0400

    feat: add function to interact with menu for logout

--- app/src/main/java/com/example/backfrontmovie/app/view/MainActivity.kt
+++ app/src/main/java/com/example/backfrontmovie/app/view/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.backfrontmovie.app.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityOptionsCompat
@@ -10,14 +13,19 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.backfrontmovie.R
 import com.example.backfrontmovie.app.view.details.DetailsActivity
+import com.example.backfrontmovie.app.view.login.LoginActivity
+import com.example.backfrontmovie.app.viewmodel.LoginViewModel
 import com.example.backfrontmovie.databinding.ActivityMainBinding
 import com.example.data.app.repository.model.Movie
 import com.google.firebase.messaging.FirebaseMessaging
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
   private lateinit var binding: ActivityMainBinding
   private val bottomNavigation by lazy { binding.mainBottomNavigation }
+
+  private val loginViewModel by viewModel<LoginViewModel>()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -54,5 +62,25 @@ class MainActivity : AppCompatActivity() {
         Log.i("Firebase", "getToken: $token")
       }
     }
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.main_menu, menu)
+
+    return super.onCreateOptionsMenu(menu)
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    if (item.itemId == R.id.menu_logout) {
+      loginViewModel.logout()
+
+      val intent = Intent(this, LoginActivity::class.java)
+      intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+      startActivity(intent)
+
+      return true
+    }
+
+    return super.onOptionsItemSelected(item)
   }
 }
